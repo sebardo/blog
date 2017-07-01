7457c3e08b00b4fd97ceb129f3b0bcab6d0db567
Sebardo <sebastian.sasturain@gmail.com>
2017-07-01 22:10:02 +0200

    Fix upload blog

--- BlogBundle/Entity/Image.php
+++ BlogBundle/Entity/Image.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use CoreBundle\Entity\Timestampable;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Image Entity class
