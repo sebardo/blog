455bc2aaaa423873f8b973b86dd861af784090e4
Sebardo <sebastian.sasturain@gmail.com>
2017-07-21 10:27:46 +0200

    Add relation dinamic map

---
+++ BlogBundle/EventListener/DynamicRelationSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+namespace BlogBundle\EventListener;
+
+use Doctrine\Common\Persistence\Event\LoadClassMetadataEventArgs;
+use Doctrine\Common\EventSubscriber;
+
+class DynamicRelationSubscriber implements EventSubscriber
+{
+    /**
+     * @var array
+     */
+    protected $mapping;
+
+    public function __construct(array $mapping)
+    {
+        $this->mapping = $mapping;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getSubscribedEvents()
+    {
+        return array(
+            Events::loadClassMetadata,
+        );
+    }
+
+    /**
+     * @param LoadClassMetadataEventArgs $eventArgs
+     */
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        // the $metadata is the whole mapping info for this class
+        $metadata = $eventArgs->getClassMetadata();
+        $class = $metadata->getReflectionClass();
+        
+        if ($metadata->getName() == 'BlogBundle\Entity\Post') {
+            $metadata->mapManyToOne(array(
+                'targetEntity' => $this->mapping['baseactor'],
+                'fieldName' => 'actor',
+                'joinColumns' => array(array('name' => 'actor_id')),
+                'inversedBy' => 'posts',
+            ));
+        }elseif ($metadata->getName() == $this->mapping['baseactor']) {
+            $metadata->mapOneToMany(array(
+                'targetEntity' => $class->getName(),
+                'fieldName' => 'posts',
+                'mappedBy' => 'actor',
+            ));
+        }
+    }
+}
+                    
